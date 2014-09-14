388a4bd06350e0a1c6d8e228c99a50a58d57610b
Karan Goel <karan@goel.im>
2014-09-14 02:42:04 -0700

    add light, fix tons of bugs

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-var tessel = require('tessel'); // import tessel
+var request = require('request');
+var tessel = require('tessel');
 
 var led = {
   green : tessel.led[0].output(0), // output(1) -> Initial on
   blue  : tessel.led[1].output(0), // output(0) -> Initial off
-} 
+}
 
 // baudrate must match the baudrate set in the Arduino file
 uartBridge = tessel.port['D'].UART({baudrate: 9600}); 
 
-uartBridge.on('data', function(data){
+uartBridge.on('data', function(data) {
   var newValue = parseInt(data[0]);
+
   led.green.toggle();
+  
+  var url = 'https://graph.api.smartthings.com/api/smartapps/installations/40f0fc12-7ea9-4b62-86e7-30588c71df64/babyincradle';
+  request({
+    url: url,
+    method: 'PUT',
+    headers: { "Authorization": "Bearer 8b35f1bd-d58d-41a2-89b1-4c4608fa4e54" },
+    body: { 'isPressure': newValue },
+    json: true,
+    timeout: 1000
+  }, function(err, resp, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(resp.statusCode);
+    }
+  });
 });
+
--- react_to_pressure/react_to_pressure.ino
+++ react_to_pressure/react_to_pressure.ino
@@ -3,6 +3,7 @@
 #define txPin 11 // connect to RX of other device
 
 int sensorPin = 2;    // select the input pin for the potentiometer
+int led = 13;
 int sensorValue = 0;  // variable to store the value coming from the sensor
 int oldSensorValue = 0;  // what was the last seen value
 
@@ -25,10 +26,24 @@ void loop() {
     Serial.print(sensorValue);
     Serial.print("\n");
     tesselBridge.write(sensorValue);
+    if (sensorValue == 1) {
+      flashRed();
+    } else {
+      stopFlashRed(); 
+    }
     oldSensorValue = sensorValue;
     delay(1000); // to make sure we don't have false positives
   }
   // stop the program for <sensorValue> milliseconds:
-  delay(100);
+  delay(500);
 }
 
+void flashRed() {
+  pinMode(led, OUTPUT);
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+}
+
+void stopFlashRed() {
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);   // turn the LED on (HIGH is the voltage level)
+}
